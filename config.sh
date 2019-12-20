033cf9c1b9cc04a78eb93b940d9d4c4959f45774
Gonzalo Martinez <gonzalo.martinez.larumbe@hp.com>
2019-12-20 19:57:33 +0100

    Update config.sh file with repo renaming

--- config.sh
+++ config.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Rename repos
+mv ~/emacsconf ~/.elisp
+mv ~/emacsconf_priv ~/.elisp_private
+
+# Create symlinks inside .emacs.d
 ln -sf ~/.elisp_private ~/.emacs.d/.elisp_private
 ln -sf ~/.elisp/larumbe/init.el ~/.emacs.d/init.el
