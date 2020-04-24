744a2fd2c704df5bf3e332a19d8783d6bee31b8b
Gonzalo Martinez <gonzalomlarumbe@gmail.com>
2020-04-24 13:34:23 +0200

    Add overriden vhdl-mode from Emacs 26.1 builtin

--- larumbe/vhdl-settings.el
+++ larumbe/vhdl-settings.el
@@ -2,37 +2,33 @@
 ;; VHDL setup ;;
 ;;;;;;;;;;;;;;;;
 
-;;; Variables
-(setq vhdl-clock-edge-condition (quote function))
-(setq vhdl-company-name "")
-(setq vhdl-conditions-in-parenthesis t)
-(setq vhdl-default-library "xil_defaultlib")
-(setq vhdl-end-comment-column 120)
-(setq vhdl-platform-spec "Debian 9.1")
-(setq vhdl-reset-kind (quote sync))
-(setq vhdl-speedbar-auto-open nil)
-(setq vhdl-standard '(08 nil))
-(setq vhdl-project "AXI Interface Converter")
-
-
-;;; Hooks
-(defun my-vhdl-hook ()
-  (setq tab-width 4)                    ; TAB Width for indentation
-  (setq indent-tabs-mode nil)           ; Replace TAB with Spaces when indenting
-  (setq vhdl-basic-offset 4)
+;;; Basic settings
+(use-package vhdl-mode
+  :load-path "~/.elisp/larumbe/own-modes/override"
+  :bind (:map vhdl-mode-map
+              ("<f5>" . vhdl-compile)
+              ("<C-iso-lefttab>" . 'insert-tab-vhdl)
+              ("C-M-<tab>" . remove-tab-vhdl)
+              )
+  :init   ; INFO: Requires to be set before loading package in order to variables like faces to take effect
+  (setq vhdl-clock-edge-condition (quote function))
+  (setq vhdl-company-name "")
+  (setq vhdl-conditions-in-parenthesis t)
+  (setq vhdl-default-library "xil_defaultlib")
+  (setq vhdl-end-comment-column 120)
+  (setq vhdl-platform-spec "Debian 9.1")
+  (setq vhdl-reset-kind (quote sync))
+  (setq vhdl-speedbar-auto-open nil)
+  (setq vhdl-standard '(08 nil))
+  (setq vhdl-project "AXI Interface Converter")
 
   (fset 'insert-tab-vhdl (kbd "C-u 4 SPC")) ; Custom 4 spaces TAB key
   (fset 'remove-tab-vhdl (kbd "C-u 4 DEL")) ; Custom remove 4 spaces TAB key
-  (local-set-key [f5] 'vhdl-compile)
-  (local-set-key (kbd "<C-iso-lefttab>") 'insert-tab-vhdl)
-  (local-set-key (kbd "C-M-<tab>") 'remove-tab-vhdl)
 
-  ;; Autocomplete sources
-  (set 'ac-sources
-             '(
-               ac-source-gtags
-               )
-             )
+  :config
+  (setq vhdl-basic-offset 4)
+  (setq tab-width 4)                    ; TAB Width for indentation
+  (setq indent-tabs-mode nil)           ; Replace TAB with Spaces when indenting
 
   ;; GHDL custom linter setup
   (setq vhdl-custom-ghdl-list
@@ -44,11 +40,16 @@
         )
   (add-to-list 'vhdl-compiler-alist vhdl-custom-ghdl-list)
   (vhdl-set-compiler "GHDL-custom")
+  (vhdl-electric-mode 1)
   )
 
+
+
+;;; Hooks
+(defun my-vhdl-hook ()
+  (set 'ac-sources '(ac-source-gtags))
+  )
 (add-hook 'vhdl-mode-hook 'my-vhdl-hook)
-(add-hook 'vhdl-mode-hook 'vhdl-electric-mode)
-(add-hook 'vhdl-mode-hook '(lambda () (setq compilation-error-regexp-alist (delete 'gnu compilation-error-regexp-alist))))
 
 
 
