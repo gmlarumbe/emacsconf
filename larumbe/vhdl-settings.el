ce34326abbb53717aa9bb55b41185d03f0abcacf
Gonzalo Martinez <gonzalomlarumbe@gmail.com>
2020-04-26 02:08:43 +0200

    Implement vhdl jump to module at point and to parent module

--- larumbe/vhdl-settings.el
+++ larumbe/vhdl-settings.el
@@ -6,8 +6,12 @@
 (use-package vhdl-mode
   :load-path "~/.elisp/larumbe/own-modes/override"
   :bind (:map vhdl-mode-map
+              ("C-M-a"           . vhdl-beginning-of-defun)
+              ("C-M-e"           . vhdl-end-of-defun)
               ("C-M-d"           . larumbe/find-vhdl-module-instance-fwd)
               ("C-M-u"           . larumbe/find-vhdl-module-instance-bwd)
+              ("C-M-p"           . vhdl-backward-same-indent)
+              ("C-M-n"           . vhdl-forward-same-indent)
               ("<f5>"            . vhdl-compile)
               ("<C-iso-lefttab>" . insert-tab-vhdl)
               ("C-M-<tab>"       . remove-tab-vhdl)
@@ -45,6 +49,10 @@
   (add-to-list 'vhdl-compiler-alist vhdl-custom-ghdl-list)
   (vhdl-set-compiler "GHDL-custom")
   (vhdl-electric-mode 1)
+
+  (bind-chord "\\\\" #'larumbe/vhdl-jump-to-module-at-point vhdl-mode-map)
+  (when (executable-find "ag")
+    (bind-chord "\|\|" #'larumbe/vhdl-find-parent-module vhdl-mode-map))
   )
 
 
@@ -54,7 +62,6 @@
   (set 'ac-sources '(ac-source-gtags))
   )
 (add-hook 'vhdl-mode-hook 'my-vhdl-hook)
-
 
 
 ;;; Gtags
@@ -72,7 +79,8 @@
 
 
 
-;;; Custom functions
+;;; Navigation
+(defvar larumbe/vhdl-entity-re "^\\s-*\\(entity\\)\\s-+\\(\\(\\w\\|\\s_\\)+\\)")
 (defvar larumbe/vhdl-blank-opt-re "[[:blank:]\n]*")
 (defvar larumbe/vhdl-blank-mand-re "[[:blank:]\n]+")
 (defvar larumbe/vhdl-identifier-re "[a-zA-Z_][a-zA-Z0-9_-]*")
@@ -130,7 +138,7 @@ LIMIT argument is included to allow the function to be used to fontify VHDL buff
 
 ;;; Imenu
 (defconst larumbe/vhdl-imenu-generic-expression
-  '(
+  `(
     ("Subprogram"
      "^\\s-*\\(\\(\\(impure\\|pure\\)\\s-+\\|\\)function\\|procedure\\)\\s-+\\(\"?\\(\\w\\|\\s_\\)+\"?\\)"
      4)
@@ -159,7 +167,7 @@ LIMIT argument is included to allow the function to be used to fontify VHDL buff
      "^\\s-*\\(architecture\\)\\s-+\\(\\(\\w\\|\\s_\\)+\\s-+of\\s-+\\(\\w\\|\\s_\\)+\\)"
      2)
     ("Entity"
-     "^\\s-*\\(entity\\)\\s-+\\(\\(\\w\\|\\s_\\)+\\)"
+     ,larumbe/vhdl-entity-re
      2)
     ("Context"
      "^\\s-*\\(context\\)\\s-+\\(\\(\\w\\|\\s_\\)+\\)"
@@ -175,3 +183,49 @@ LIMIT argument is included to allow the function to be used to fontify VHDL buff
        larumbe/vhdl-imenu-generic-expression)
   (when (and vhdl-index-menu (fboundp 'imenu))
     (imenu-add-to-menubar "Index")))
+
+
+;;; Jump to modules
+;; INFO: By default `which-func' would get Info from Imenu.
+;;  - This was far faster for which-func updating than using the custom `larumbe/find-vhdl-module-instance-bwd' function
+;;  and performing analogous to modi's functions.
+
+(defun larumbe/vhdl-jump-to-module-at-point ()
+  "When in a module instance, jump to that module's definition.
+Fetched from `modi/verilog-jump-to-module-at-point'"
+  (interactive)
+  (if (and (executable-find "global")
+           (projectile-project-root))
+      (let ((module (gethash (get-buffer-window) which-func-table))) ; INFO: modi/verilog analogous uses `which-func-current' but it didn't work well here...
+        (if (save-excursion
+              (larumbe/find-vhdl-module-instance-bwd))
+            (ggtags-find-tag-dwim module))
+        ;; Do `pop-tag-mark' if this command is called when the
+        ;; point in *not* inside a verilog instance.
+        (pop-tag-mark))
+    (user-error "Executable `global' is required for this command to work")))
+
+
+(defun larumbe/vhdl-find-parent-module ()
+  "Find the places where the current VHDL module is instantiated in the project.
+Fetched from `modi/verilog-find-parent-module'"
+  (interactive)
+  (let (module-name
+        module-instance-pcre
+        regexp)
+    ;; Get entity name
+    (save-excursion
+      (re-search-backward larumbe/vhdl-entity-re))
+    (setq module-name (match-string-no-properties 2))
+    ;; Reformat regexp to PCRE:
+    ;; INFO: Regexp fetched from `larumbe/vhdl-instance-re', replaced "\\s-" with "[ ]", and dismissing \n to allow for easy elisp to pcre conversion
+    ;; Otherwise it was getting a real hell since `rxt-elisp-to-pcre' does not seem to support newlines conversion.
+    (setq regexp (concat "^[ ]*\\([a-zA-Z_][a-zA-Z0-9_-]*\\)[ ]*:[ ]*"
+                         "\\(\\(component[ ]+\\|configuration[ ]+\\|\\(entity[ ]+\\([a-zA-Z_][a-zA-Z0-9_-]*\\).\\)\\)\\)?"
+                         "\\(" module-name "\\)[ ]*"))
+    (setq module-instance-pcre (rxt-elisp-to-pcre regexp))
+    (let* ((ag-arguments ag-arguments)) ;Save the global value of `ag-arguments'
+      ;; Search only through vhdl type files.
+      ;; See "ag --list-file-types".
+      (add-to-list 'ag-arguments "--vhdl" :append)
+      (ag-regexp module-instance-pcre (projectile-project-root)))))
