f5b33e0954cd640404fd941416bf2a60664645f1
Gonzalo Martinez <gonzalomlarumbe@gmail.com>
2020-04-27 19:04:46 +0200

    Add hydra support for vhdl-mode based on vhdl-template-* functions

--- larumbe/vhdl-settings.el
+++ larumbe/vhdl-settings.el
@@ -15,6 +15,7 @@
               ("<f5>"            . vhdl-compile)
               ("<C-iso-lefttab>" . insert-tab-vhdl)
               ("C-M-<tab>"       . remove-tab-vhdl)
+              ("C-c C-t"         . hydra-vhdl-template/body)
               )
   :init   ; INFO: Requires to be set before loading package in order to variables like faces to take effect
   (fset 'insert-tab-vhdl (kbd "C-u 4 SPC")) ; Custom 4 spaces TAB key
@@ -47,7 +48,7 @@
         )
   (add-to-list 'vhdl-compiler-alist vhdl-custom-ghdl-list)
   (vhdl-set-compiler "GHDL-custom")
-  (vhdl-electric-mode 1)
+  (vhdl-electric-mode -1)
   ;; Bind chords
   (bind-chord "\\\\" #'larumbe/vhdl-jump-to-module-at-point vhdl-mode-map)
   (when (executable-find "ag")
@@ -285,3 +286,106 @@ Returns a list of directories from current VHDL opened files. Useful for `ghdl' 
         (when (string-equal major-mode "vhdl-mode")
           (add-to-list 'vhdl-opened-dirs default-directory))))
     (eval 'vhdl-opened-dirs)))
+
+
+
+(defun larumbe/vhdl-insert-instance-from-file (file)
+  "Insert instance at point from file.
+Copied and adapted from `larumbe/verilog-insert-instance-from-file'"
+  (interactive "FSelect entity from file:")
+  (let* ((entity-name (with-temp-buffer
+                        (insert-file-contents file)
+                        (if (re-search-forward larumbe/vhdl-entity-re nil t)
+                            (progn
+                              (save-match-data
+                                (vhdl-port-copy))
+                              (match-string-no-properties 2))
+                          (error "No VHDL entity found in that file!"))))
+         (instance-name (read-string "Instance-name: " (concat "I_" (upcase entity-name)))))
+    (vhdl-port-paste-instance instance-name)))
+
+
+(defun larumbe/vhdl-insert-testbench-from-file (file)
+  "Create testbench from entity of selected file in current buffer directory."
+  (interactive "FSelect entity from file:")
+  (let* ((entity-name (with-temp-buffer
+                        (insert-file-contents file)
+                        (if (re-search-forward larumbe/vhdl-entity-re nil t)
+                            (progn
+                              (save-match-data
+                                (vhdl-port-copy))
+                              (match-string-no-properties 2))
+                          (error "No VHDL entity found in that file!")))))
+    (if (buffer-file-name)
+        (vhdl-port-paste-testbench)
+      (error "Not visiting a file. TB is created in current file directory!!"))))
+
+
+
+
+;;;; Hydra
+(defhydra hydra-vhdl-template (:color blue
+                                      :hint nil)
+  "
+RTL                   TESTBENCH             COMMON            PACKAGES
+^^
+_ps_: process seq       _@_:  clocked wait      _for_: for          _pb_: package body
+_pc_: process comb      _pr_: procedure         _fn_: function      _pd_: package declaration
+_en_: entity            _rp_: report            _sg_: signal        _pkg_: library package
+_ac_: architecture      _as_: assert            _va_: variable
+_gn_: generate          _fl_: file              _ct_: constant
+_cp_: component         _TS_: TestBench         _ge_: generic
+_at_: attribute         ^^                      _ty_: type
+_IS_: Instance          ^^                      _al_: alias
+^^                      ^^                      _if_: if-then
+^^                      ^^                      _cc_: case
+^^                      ^^                      _wh_: while
+^^                      ^^                      _hd_: header
+"
+  ;;;;;;;;;
+  ;; RTL ;;
+  ;;;;;;;;;
+  ("ps"  (vhdl-template-process-seq))
+  ("pc"  (vhdl-template-process-comb))
+  ("en"  (vhdl-template-entity))
+  ("ac"  (vhdl-template-architecture))
+  ("gn"  (vhdl-template-generate))
+  ("cp"  (vhdl-template-component))
+  ("at"  (vhdl-template-attribute))
+  ("IS"  (call-interactively 'larumbe/vhdl-insert-instance-from-file))
+
+  ;;;;;;;;;;;;;;;
+  ;; TestBench ;;
+  ;;;;;;;;;;;;;;;
+  ("@"  (vhdl-template-clocked-wait))
+  ("pr" (vhdl-template-procedure))
+  ("rp" (vhdl-template-report))
+  ("as" (vhdl-template-assert))
+  ("fl" (vhdl-template-file))
+  ("TS"  (call-interactively 'larumbe/vhdl-insert-testbench-from-file))
+
+  ;;;;;;;;;;;;
+  ;; Common ;;
+  ;;;;;;;;;;;;
+  ("for" (vhdl-template-for))
+  ("fn"  (vhdl-template-function))
+  ("sg"  (vhdl-template-signal))
+  ("va"  (vhdl-template-variable))
+  ("ct"  (vhdl-template-constant))
+  ("ge"  (vhdl-template-generic))
+  ("ty"  (vhdl-template-type))
+  ("al"  (vhdl-template-alias))
+  ("if"  (vhdl-template-if-then))
+  ("cc"  (vhdl-template-case))
+  ("wh"  (vhdl-template-while-loop))
+  ("hd"  (vhdl-template-header))
+
+  ("pb"  (vhdl-template-package-body))
+  ("pd"  (vhdl-template-package-decl))
+  ("pkg" (call-interactively 'vhdl-template-insert-package))
+
+  ;;;;;;;;;;;;
+  ;; Others ;;
+  ;;;;;;;;;;;;
+  ("q"   nil nil :color blue)
+  ("C-g" nil nil :color blue))
