7e27bc4879242d989d3ccfc9feb638c2589d004f
Gonzalo Martinez <gonzalomlarumbe@gmail.com>
2020-12-04 13:41:17 +0100

    Remove custom-file from version control (moved to user emacs dir)

--- larumbe/config-basic.el
+++ larumbe/config-basic.el
@@ -3,15 +3,17 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Basic settings
 ;;;; Basics
-(server-start)                           ; Server start for emacsclient support
-(load-theme 'deeper-blue t)              ; Load theme
-(desktop-save-mode 1)                    ; Autosave Desktop
-(setq confirm-kill-emacs #'y-or-n-p)     ; Avoid closing Emacs unexpectedly (helm prefix C-x c)
-(setq inhibit-startup-screen t)          ; Inhibit startup screen
-(setq disabled-command-function 'ignore) ; Enable all commands
-(defalias 'yes-or-no-p 'y-or-n-p)        ; Globally set y-or-n-p
-(setq custom-file "~/.elisp/larumbe/custom-file.el")
-(load custom-file)
+(server-start)                                 ; Server start for emacsclient support
+(setq custom-file "~/.emacs.d/custom-file.el") ; Custom file does not need to be in version control.
+(unless (file-exists-p custom-file)            ; It will only hold a list with safe variables, `package-selected-packages' for autoremove and custom set variables.
+  (write-region "" nil custom-file))           ; All of these are actually local to a machine.
+(load custom-file)                             ; Create file if it doesn't exist in emacs directory, and load it
+(load-theme 'deeper-blue t)                    ; Load theme
+(desktop-save-mode 1)                          ; Autosave Desktop
+(setq confirm-kill-emacs #'y-or-n-p)           ; Avoid closing Emacs unexpectedly (helm prefix C-x c)
+(setq inhibit-startup-screen t)                ; Inhibit startup screen
+(setq disabled-command-function 'ignore)       ; Enable all commands
+(defalias 'yes-or-no-p 'y-or-n-p)              ; Globally set y-or-n-p
 
 ;;;; Window/Frame Display
 (menu-bar-mode -1)   ; Disable Menu bar
