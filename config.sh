99d8b604a4005b48b5f91819b728024ffecaf304
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2020-12-07 15:29:23 +0100

    Workaround config.sh to fix Post Run checkout CI GitHub Action

--- config.sh
+++ config.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 # Rename repos
-CUR_DIR=$PWD
-cd $HOME
+# CUR_DIR=$PWD
 
 mkdir -p ${HOME}/.emacs.d
-mv ${CUR_DIR} ${HOME}/.elisp
+
+ln -s $PWD ~/.elisp
+ln -s larumbe/init.el ${HOME}/.emacs.d/init.el
+
+
+# mv ${CUR_DIR} ${HOME}/.elisp
 # mv ~/emacsconf_priv ~/.elisp_private
 
 # Create symlinks inside .emacs.d
-ln -sf ${HOME}/.elisp/larumbe/init.el ${HOME}/.emacs.d/init.el
+# ln -sf ${HOME}/.elisp/larumbe/init.el ${HOME}/.emacs.d/init.el
