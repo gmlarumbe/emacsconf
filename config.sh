9dcf352011b717c7edcf410a8fe2378cd0c46961
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2020-12-07 14:22:14 +0100

    Update configuration scripts for startup

--- config.sh
+++ config.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Rename repos
-mv ~/emacsconf ~/.elisp
-mv ~/emacsconf_priv ~/.elisp_private
+CUR_DIR=$PWD
+cd $HOME
+
+mkdir -p ${HOME}/.emacs.d
+mv ${CUR_DIR} ${HOME}/.elisp
+# mv ~/emacsconf_priv ~/.elisp_private
 
 # Create symlinks inside .emacs.d
-ln -sf ~/.elisp_private ~/.emacs.d/.elisp_private
-ln -sf ~/.elisp/larumbe/init.el ~/.emacs.d/init.el
+ln -sf ${HOME}/.elisp/larumbe/init.el ${HOME}/.emacs.d/init.el
