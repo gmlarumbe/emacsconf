fc5a49d1c079308036aea80ba650a87b96036f41
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2020-12-09 12:08:14 +0100

    Fix flycheck warnings of org-settings

--- larumbe/org-settings.el
+++ larumbe/org-settings.el
@@ -6,14 +6,13 @@
 (use-package org
   :ensure nil
   :bind (:map org-mode-map
-              ("C-c l" . org-store-link)
-              ("C-c a" . org-agenda)
               ("C-c c" . org-capture)
               ("C-c b" . org-iswitchb)
               ("M-."   . org-open-at-point)  ; Override xref-find-definitions, used now to follow internal/external links/tags
               ("M-,"   . org-mark-ring-goto) ; Override xref-pop-marker-stack, used now to pop back links
-              ("C-,"   . nil) ; Unamps org-cycle-agenda-files to free `larumbe/ansi-term'
-              )
+              ("C-,"   . nil)                ; Unamps org-cycle-agenda-files to free `larumbe/ansi-term'
+              ("C-c l" . org-store-link)
+              ("C-c a" . org-agenda))
   :config
   (setq org-log-done 'time)
   (setq org-agenda-files (list "~/TODO.org"))
@@ -25,19 +24,18 @@
           ("IN PROGRESS" . "yellow")
           ("CANCELED"    . (:foreground "blue" :weight bold))
           ("POSTPONED"   . "cyan")
-          ("INFO"        . "light blue")
-          )))
+          ("INFO"        . "light blue")))
 
+  (defun larumbe/org-show-todos-agenda ()
+    "Show `org-mode' TODOs and agenda."
+    (interactive)
+    (let* ((buf  "TODO.org")
+           (file (concat "~/" buf)))
+      (when (not (get-buffer buf))
+        (find-file file))
+      (switch-to-buffer buf)
+      (call-interactively #'org-agenda-list))))
 
-(defun larumbe/org-show-todos-agenda ()
-  "Show `org-mode' TODOs and agenda."
-  (interactive)
-  (let* ((buf  "TODO.org")
-         (file (concat "~/" buf)))
-    (when (not (get-buffer buf))
-      (find-file file))
-    (switch-to-buffer buf)
-    (call-interactively #'org-agenda-list)))
 
 
 (provide 'org-settings)
