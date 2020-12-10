66369fff5067f4621ac61a7431b658c4311d67a0
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2020-12-10 22:16:37 +0100

    Fix warnings of verilog-imenu

--- larumbe/verilog-settings/verilog-imenu.el
+++ larumbe/verilog-settings/verilog-imenu.el
@@ -1,11 +1,8 @@
 ;;; verilog-imenu.el --- Verilog Imenu  -*- lexical-binding: t -*-
 ;;; Commentary:
-;;; Code:
-
-;;; Imenu
 ;; INFO: Issues with instance detection
 ;;
-;; 1 - Imenu must work on current buffer. Creates an alist of (elements . #<mark pos at buffer>)
+;; 1 - Imenu must work on current buffer.  Creates an alist of (elements . #<mark pos at buffer>)
 ;;     Therefore, must be executed on the buffer on which it will have the effect (cannot use with-temp-buffer in a buffer with no comments
 ;;     and trying to reassociate afterwards)
 ;;
@@ -14,12 +11,12 @@
 ;; 3 - It is not possible to work with (with-comments-hidden) since it makes comments invisible, and imenu ignores invisible characters
 ;;     by looking for the next non-invisible regexp, since `re-search-forward' cannot ignore invisible, just skip to the next.
 ;;     The problem is that instances regexp are multiline, and if an unexpected character such as comment with semicolon appears, it won't
-;;     be recognized, and there wont be any chance of skip to the next. It will be missed.
+;;     be recognized, and there wont be any chance of skip to the next.  It will be missed.
 ;;
 ;; 4 - A first solution seemed to be executing `imenu' after erasing comments from current buffer and then returning it to its initial state
 ;;     But that would require use of `larumbe/delete-comments-from-buffer' (very slow) and `undo', with some issues programatically.
-;;     That would need  to be done with `larumbe/find-verilog-module-instance-fwd' as well. The profit would not be worth the effort due to
-;;     an extreme fall in performance.
+;;     That would need  to be done with `larumbe/find-verilog-module-instance-fwd' as well.
+;;     The profit would not be worth the effort due to an extreme fall in performance.
 ;;
 ;; 5 - Best solution is to create a function that checks if there are problematic regexps in a verilog file, and set is as a hook every time
 ;;     a file is opened, or Imenu is executed.
@@ -27,16 +24,20 @@
 ;;
 ;; INFO: There are 3 ways of creating the index-alist for Imenu mode (from simpler to more complex):
 ;;
-;;   1 - Define `imenu-generic-expression' (categories and regexps). This is the most common and default one.
+;;   1 - Define `imenu-generic-expression' (categories and regexps).  This is the most common and default one.
 ;;
 ;;   2 - Define `imenu-prev-index-position-function' and `imenu-extract-index-name-function'.
-;;       If these variables are defined, the imenu-list creation function uses them to find the tags. For example:
-;;       (setq imenu-prev-index-position-function 'larumbe/verilog-imenu-prev-index-position-function)
-;;       (setq imenu-extract-index-name-function 'larumbe/verilog-imenu-extract-index-name)
+;;       If these variables are defined, the imenu-list creation function uses them to find the tags.  For example:
+;;
+;;         (setq imenu-prev-index-position-function 'larumbe/verilog-imenu-prev-index-position-function)
+;;         (setq imenu-extract-index-name-function 'larumbe/verilog-imenu-extract-index-name)
+;;
 ;;       Check `larumbe/verilog-imenu-prev-index-position-function' and `larumbe/verilog-imenu-extract-index-name'
 ;;
 ;;   3 - Redefine `imenu-create-index-function' to make a custom more complex alist (e.g a tree recursively for nested classes)
-;;       This is the most complex and the one used in python mode. Check `larumbe/verilog-imenu-index'
+;;       This is the most complex and the one used in python mode.  Check `larumbe/verilog-imenu-index'.
+;;
+;;; Code:
 
 
 ;; Search by regexp: Used as regexps in `larumbe/verilog-imenu-generic-expression'
@@ -53,7 +54,7 @@
 (defvar larumbe/verilog-class-re    "\\(?1:\\(?:\\(?:virtual\\|interface\\)\\s-+\\)?class\\)\\s-+\\(?2:[A-Za-z_][A-Za-z0-9_]+\\)")
 (defvar larumbe/verilog-top-re      "\\(?1:package\\|program\\|module\\)\\(\\s-+automatic\\)?\\s-+\\(?2:[A-Za-z_][A-Za-z0-9_]+\\)")
 
-(setq larumbe/verilog-imenu-generic-expression
+(defvar larumbe/verilog-imenu-generic-expression
       `((nil                ,larumbe/verilog-imenu-top-re 2)
         ("*Localparams*"    ,larumbe/verilog-imenu-localparam-re 2)
         ("*Defines*"        ,larumbe/verilog-imenu-define-re 1)
@@ -62,8 +63,7 @@
         ("*Always blocks*"  ,larumbe/verilog-imenu-always-re 4)
         ("*Initial blocks*" ,larumbe/verilog-imenu-initial-re 3)
         ("*Task/Func*"      larumbe/find-verilog-task-function-outside-class-bwd 2)
-        ("*Instances*"      larumbe/find-verilog-module-instance-bwd 1)  ;; Use capture group index 2 if want to get instance name instead
-        ))
+        ("*Instances*"      larumbe/find-verilog-module-instance-bwd 1)))  ;; Use capture group index 2 if want to get instance name instead
 
 
 (defun larumbe/verilog-imenu-format-class-item-label (type name)
@@ -90,7 +90,7 @@ If optional ADD, add the parent with TYPE, NAME and POS to the TREE."
 
 
 (defun larumbe/verilog-imenu-build-class-tree (&optional tree)
-  "Build the imenu alist tree.
+  "Build the imenu alist TREE.
 Coded to work with verification files with CLASSES and METHODS.
 Adapted from `python-mode' imenu build-tree function."
   (save-restriction
@@ -124,9 +124,12 @@ Adapted from `python-mode' imenu build-tree function."
 
 
 (defun larumbe/verilog-imenu-classes-index ()
-  "Obtain entries of tasks/functions WITHIN classes.
-NOTE: Tasks/functions outside classes are obtained with a custom function search in the generic imenu-generic-function stage.
-INFO: Detection of nested classes is unsupported and leads to bad detection of class tasks/functions."
+  "Obtain entries of tasks/functions within classes.
+
+INFO: Tasks/functions outside classes are obtained with a custom function search
+in the generic imenu-generic-function stage.
+INFO: Detection of nested classes is unsupported and leads to bad detection of class
+tasks/functions."
   (save-excursion
     (goto-char (point-max))
     (let ((index)
@@ -140,8 +143,9 @@ INFO: Detection of nested classes is unsupported and leads to bad detection of c
 
 (defun larumbe/verilog-imenu-index ()
   "Custom index builder for Verilog Imenu.
-First creates a list with the entries that correspond to *Classes* tag by performing a recursive search.
-Afterwards it appends the contents of the list obtained by using the imenu generic function. "
+First creates a list with the entries that correspond to *Classes* tag by
+performing a recursive search.  Afterwards it appends the contents of the
+list obtained by using the imenu generic function."
   (append
    (larumbe/verilog-imenu-classes-index)
    (imenu--generic-function larumbe/verilog-imenu-generic-expression)))
@@ -166,10 +170,10 @@ If optional FIRST is used, then shows first block (Verilog *instances/interfaces
   (interactive)
   (if (string-equal major-mode "imenu-list-major-mode")
       (progn
-        (beginning-of-buffer)
+        (goto-char (point-min))
         (while (< (point) (point-max))
           (hs-hide-block)
-          (next-line))
+          (forward-line))
         (beginning-of-buffer)
         ;; If there is an optional argument, unfold first block
         (when first
@@ -187,8 +191,6 @@ If optional FIRST is used, then shows first block (Verilog *instances/interfaces
   "Function to extract the tag."
   (verilog-forward-syntactic-ws)
   (thing-at-point 'symbol t))
-
-
 
 
 (provide 'verilog-imenu)
