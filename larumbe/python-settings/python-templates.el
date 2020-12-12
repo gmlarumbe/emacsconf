d9b0f222c0f5ea6841dd9a0ecad75c8af829a082
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2020-12-12 20:19:52 +0100

    Refactor python with yasnippets

---
+++ larumbe/python-settings/python-templates.el
@@ -0,0 +1,13 @@
+;;; python-templates.el --- Python Templates  -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+;;;; Hydra+Yasnippet
+(defun larumbe/hydra-python-placeholder ()
+  "Placeholder for Hydra+Yasnippet with Python."
+  (interactive)
+  (message "Hydra+Yasnippet not implemented yet. Use `C-c C-s' to insert snippet."))
+
+(provide 'python-templates)
+
+;;; python-templates.el ends here
