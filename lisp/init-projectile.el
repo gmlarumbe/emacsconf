3c0c322ca1027c69728936a90cacaf6413c2fa0f
Gonzalo Martinez <gonzalomlarumbe@gmail.com>
2021-01-15 11:21:39 +0100

    Adapt projectile settings to own projects

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -22,34 +22,24 @@
              larumbe/projectile-mode)
 
   :config
-  (add-to-list 'projectile-project-root-files-bottom-up ".repo")
+  (setq projectile-enable-caching t) ; Enable caching, otherwise `projectile-find-file' is really slow for large projects.
+
   (add-to-list 'projectile-globally-ignored-directories "*.repo") ; https://github.com/bbatsov/projectile/issues/1250
+
   (let ((ignore-targets '("bundle_targets" "sim_targets" "syn_targets" "doc_targets" "version_targets")))
     (dolist (dir ignore-targets)
       (add-to-list 'projectile-globally-ignored-directories dir)))
+
   (setq projectile-indexing-method 'hybrid) ; `alien' is the fastest indexing method (default), but ignores .projectile ignores
   (setq projectile-completion-system 'helm)
   (setq projectile-mode-line-prefix " P") ; Modeline
   (setq projectile-mode-line-function #'larumbe/projectile-custom-mode-line)
 
-
-  ;; Fetched from modi
-  (setq projectile-enable-caching t) ; Enable caching, otherwise
-                                        ; `projectile-find-file' is really slow
-                                        ; for large projects.
-  (dolist (item '("GTAGS" "GRTAGS" "GPATH"))
-    (add-to-list 'projectile-globally-ignored-files item))
-
-  ;; Git projects should be marked as projects in top-down fashion,
-  ;; so that each git submodule can be a projectile project.
-  (setq projectile-project-root-files-bottom-up
-        (delete ".git" projectile-project-root-files-bottom-up))
-  (add-to-list 'projectile-project-root-files ".git")
-
-  (setq projectile-project-root-files-functions
-        '(projectile-root-local
-          projectile-root-top-down ; First look for projects in top-down order
-          projectile-root-bottom-up)) ; Then in bottom-up order
+  ;; Default search order is defined by functions of variable `projectile-project-root-functions':
+  (defvar larumbe/projectile-project-root-files '("GTAGS" ".repo" ".git" ".svn")) ; In order of precedence. If none of this works, use variable `projectile-project-root'
+  (setq projectile-project-root-files                    larumbe/projectile-project-root-files) ; Top-down
+  (setq projectile-project-root-files-bottom-up          larumbe/projectile-project-root-files) ; Bottom-up
+  (setq projectile-project-root-files-top-down-recurring larumbe/projectile-project-root-files) ; Top-down recurring
 
 
   (defun larumbe/projectile-custom-mode-line ()
