880e702f7d5c8e9b5d2158168d3974d9367d92c8
Gonzalo Martinez <gonzalomlarumbe@gmail.com>
2021-08-31 19:08:09 +0200

    Insert upper header string after org-insert-todo-heading

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -15,8 +15,9 @@
               ("C-c l" . org-store-link)
               ("C-c a" . org-agenda))
   :bind (("C-x l" . larumbe/org-show-todos-agenda))
-  :hook ((org-agenda-mode . larumbe/org-mode-hook)
-         (org-mode        . larumbe/org-mode-hook))
+  :hook ((org-agenda-mode    . larumbe/org-mode-hook)
+         (org-mode           . larumbe/org-mode-hook)
+         (org-insert-heading . larumbe/org-insert-current-header))
   :config
   (setq org-log-done 'time)
   (setq org-agenda-files (list "~/TODO.org"))
@@ -46,7 +47,30 @@
   (defun larumbe/org-mode-hook ()
     "`org-mode' own hook."
     (interactive)
-    (hardcore-mode -1)))
+    (hardcore-mode -1))
+
+
+  (defun larumbe/org-insert-current-header ()
+    "Insert current header of highest hierarchy.
+Meant to be used as a hook for `org-insert-heading-hook'"
+    (interactive)
+    (let (outline-path header text)
+      (setq outline-path (org-get-outline-path))
+      ;; Corner case when it's the first heading after a top-level
+      (unless outline-path
+        (save-excursion
+          (org-previous-visible-heading 1)
+          (setq outline-path (org--get-outline-path-1))))
+      ;; Set text to be inserted
+      (setq header (car outline-path))
+      (setq text (concat header ": "))
+      (insert text)))
+
+
+  ;; By default, `org-insert-todo-heading' inserts the TODO after
+  ;; the `org-insert-heading-hook'. This advice moves the pointer
+  ;; to the end of the line, making it ready to write afterwards.
+  (advice-add 'org-insert-todo-heading :after #'org-end-of-line))
 
 
 
