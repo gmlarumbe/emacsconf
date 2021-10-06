460018107737f43e7e52a397b5c980ba7b4f97f2
Gonzalo Martinez <gonzalomlarumbe@gmail.com>
2021-10-06 21:26:30 +0200

    Use ripgrep to fetch projectile files for generic projects (repo)

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -60,7 +60,48 @@ ARG will be passed to `projectile-mode' wrapped function."
     (interactive)
     (if larumbe/projectile-enable
         (projectile-mode arg)
-      (projectile-mode -1))))
+      (projectile-mode -1)))
+
+
+  ;; https://emacs.stackexchange.com/questions/16497/how-to-exclude-files-from-projectile
+  ;; Inspired also by kmodi/setup-files/setup-projectile.el:71
+  (defvar larumbe/gitignore-global-file (larumbe/path-join (getenv "HOME") ".gitignore_global"))
+
+  (defvar larumbe/rg-arguments
+    `("--no-ignore-vcs"     ; Ignore files/dirs ONLY from `.ignore'
+      "--line-number"       ; Line numbers
+      "--smart-case"
+      "--follow"            ; Follow symlinks
+      "--max-columns" "150" ; Emacs doesn't handle long line lengths very well
+      "--ignore-file" ,larumbe/gitignore-global-file)
+    "Default rg arguments used in the functions in `projectile'package.")
+
+  (defun larumbe/projectile-rg-command ()
+    "Fetched from `modi/advice-projectile-use-rg.
+Use `rg' for getting a list of all files in the project."
+    (mapconcat #'shell-quote-argument
+               (append '("rg")
+                       larumbe/rg-arguments
+                       '("--null" ; Output null separated results
+                         ;; Get names of all the to-be-searched files,
+                         ;; same as the "-g ''" argument in ag.
+                         "--files"))
+               " "))
+
+  ;; Use ripgrep for repo sandboxes (considered generic project by projectile)
+  (setq projectile-generic-command
+        (cond
+         ((executable-find "rg")
+          (larumbe/projectile-rg-command))
+         ;; we prefer fd over find
+         ((executable-find "fd")
+          "fd . -0 --type f --color=never")
+         ;; fd's executable is named fdfind is some Linux distros (e.g. Ubuntu)
+         ((executable-find "fdfind")
+          "fdfind . -0 --type f --color=never")
+         ;; with find we have to be careful to strip the ./ from the paths
+         ;; see https://stackoverflow.com/questions/2596462/how-to-strip-leading-in-unix-find
+         (t "find . -type f | cut -c3- | tr '\\n' '\\0'"))))
 
 
 
