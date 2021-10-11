14a03a8ebe0ada3a8af72b37bb74fc3df925497e
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2021-10-11 11:57:34 +0200

    Update test-startup to also load early-init for CI

--- scripts/test-startup.sh
+++ scripts/test-startup.sh
@@ -7,7 +7,9 @@ echo "Attempting startup..."
 ${EMACS:=emacs} -nw --batch \
                 --eval '(let* ((debug-on-error t)
                               (user-emacs-directory (expand-file-name "~/.emacs.d/"))
+                              (early-init-file (concat (file-name-as-directory user-emacs-directory) "early-init.el"))
                               (user-init-file (concat (file-name-as-directory user-emacs-directory) "init.el")))
+                           (load-file early-init-file)
                            (load-file user-init-file)
                            (run-hooks (quote after-init-hook)))'
 echo "Startup successful"
