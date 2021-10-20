45445cfcd27f36b05ae734eca5a0ec83e3568ed2
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2021-10-20 22:12:59 +0200

    Move own git functions to my-elisp-packages

--- lisp/init-version-control.el
+++ lisp/init-version-control.el
@@ -117,112 +117,11 @@ since it needs to be set for the whole magit session, not only for the command."
   (add-hook 'git-timemachine-mode-hook '(lambda () (setq truncate-lines t))))
 
 
-
-;;;;; Aux functions
-(defun larumbe/git-find-changed-files-between-branches (rev-a rev-b)
-  "Return a list of strings with changed files between REV-A and REV-B."
-  (let ((str)
-        (buffer-name "*shell-git-diff-output*"))
-    (save-window-excursion
-      (shell-command
-       (concat "git diff --name-status " rev-a ".." rev-b)
-       buffer-name)
-      (switch-to-buffer buffer-name)
-      (goto-char (point-min))
-      (while (re-search-forward "[MAD]\t" nil t)  ; Modified/Added/Deleted = MAD
-        (replace-match ""))
-      (setq str (split-string (buffer-string)))
-      str)))
-
-
-;; https://stackoverflow.com/questions/25009453/how-to-delete-a-list-of-elements-from-another-list-in-elisp
-;; It is the same as solution 1 but using delete instead of delq, and printing the value of temp variable at the end
-(defun larumbe/git-exclude-files-before-ediff (merge-files exclude-files)
-  "Remove EXCLUDE-FILES from MERGE-FILES parameter list."
-  (let (temp)
-    (setq temp merge-files)
-    (mapc
-     (lambda (x)
-       (setq temp (delete x temp)))
-     exclude-files)
-    (setq temp temp)))  ; Return only last value after all the iterations
-
-
-(defun larumbe/git-merge-all-files-between-branches (reva revb changed-files)
-  "Ediff/Merge every file from CHANGED-FILES.
-Compares same file of revisions REVA and REVB using `magit-ediff-compare'"
-  (mapcar
-   (lambda (file)
-     (magit-ediff-compare reva revb file file))
-   changed-files))
-
-
-
-;;;;; Interactive functions
-(defvar larumbe/git-branch-files-to-exclude-from-merge
-      '(".elisp/larumbe/hp-settings.el"
-        ".elisp/larumbe/cee-settings.el"
-        ".bashrc"
-        ".ctags"
-        ".gitconfig"
-        ".xinitrc"
-        ".globalrc"
-        "TODO.org"))
-
-(defun larumbe/git-checkout-file-from-another-branch ()
-  "Used when an override needs to be performed."
-  (interactive)
-  (let (fetch-file-from-branch
-        fetch-file-to-branch
-        filename
-        files-changed)
-    (save-window-excursion
-      ;; Prepare variables according to initial setup and prompts
-      (setq fetch-file-from-branch (completing-read "Checkout file from branch: " (magit-list-branch-names)))
-      (setq fetch-file-to-branch   (completing-read "Checkout file to branch: "   (magit-list-branch-names) nil nil (magit-get-current-branch)))
-      (setq files-changed (larumbe/git-find-changed-files-between-branches fetch-file-from-branch fetch-file-to-branch))
-      (setq files-changed (larumbe/git-exclude-files-before-ediff files-changed larumbe/git-branch-files-to-exclude-from-merge))
-      (setq filename (completing-read "Choose file to checkout: " files-changed))
-      ;; And finally choose between the possible files and execute the shell-command to checkout the file
-      (shell-command
-       (concat
-        magit-git-executable " checkout " fetch-file-to-branch " && "
-        magit-git-executable " checkout " fetch-file-from-branch " " filename)))))
-
-
-(defun larumbe/git-manual-branch-ediff-merge ()
-  "Ediff manual merging of every changed file between chosen revisions."
-  (interactive)
-  (let (changed-files rev-a rev-b)
-    (setq rev-a (completing-read "RevA: " (magit-list-branch-names)))
-    (setq rev-b (completing-read "RevB: " (magit-list-branch-names) nil nil (magit-get-current-branch)))
-    ;; First part is to find which files have changed between both branches
-    (setq changed-files (larumbe/git-find-changed-files-between-branches rev-a rev-b))
-    ;; An (optional) step would be to determine which of previous files require manual merging
-    (setq changed-files (larumbe/git-exclude-files-before-ediff changed-files larumbe/git-branch-files-to-exclude-from-merge))
-    ;; Last step would be to merge manually these files
-    (larumbe/git-merge-all-files-between-branches rev-a rev-b changed-files)))
-
-
-
-
 ;;;; Repo
 (use-package repo
   :straight (:repo "canatella/repo-el"
              :fork (:repo "gmlarumbe/repo-el"))
   :bind (("C-x j" . repo-status)))
-
-
-(defun larumbe/repo-sync-sandboxes (repo-paths)
-  "Update all .repo sandboxes passed REPO-PATHS parameters.
-Meant to be used in local and remote."
-  (while repo-paths
-    (async-shell-command
-     (concat "cd " (nth 0 (car repo-paths)) " && repo sync")
-     (concat "*<" (nth 1 (car repo-paths)) ">*"))
-    (setq repo-paths (cdr repo-paths))))
-
-
 
 
 ;;;; SVN
