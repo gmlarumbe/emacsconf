9d9fcb76799b5c41d71846711ea6a5f57c66140e
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2021-10-21 16:05:28 +0200

    Update init-machine last init step

--- lisp/init-machine.el
+++ lisp/init-machine.el
@@ -6,9 +6,8 @@
 ;;   - This file will not be present in the repo
 ;;   - It will have specific machine content (e.g. EXWM enabling)
 (when (file-exists-p "~/.elisp_private/")
-  (defvar larumbe/load-path-dirs-private '("~/.elisp_private/lisp"
-                                           "~/.elisp_private/site-lisp"))
-  (larumbe/add-to-load-path larumbe/load-path-dirs-private t) ; Add recursively
+  (defvar larumbe/load-path-dirs-machine '("~/.elisp_private/lisp")) ; Required to provide `init-private' package
+  (larumbe/add-to-load-path larumbe/load-path-dirs-machine t)        ; Add recursively
   (require 'init-private))
 
 
