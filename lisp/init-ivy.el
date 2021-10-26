4f9e3cef0a4d7f7636f49c15b1bdd1aca6ebc5db
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2021-10-26 15:23:41 +0200

    Some updates to ivy

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -13,41 +13,87 @@
   (setq enable-recursive-minibuffers t) ; Allow minibuffer commands while in the minibuffer.
   ;; Do not break compatibility with Helm for switching buffers
   (if (equal larumbe/completion-framework 'ivy)
-      (ivy-mode 1)
+      (progn
+        (ivy-mode 1)
+        (use-package ivy-rich
+          :demand
+          :config
+          (ivy-rich-mode 1)))
+    ;; Else (using helm)
     (ivy-mode -1))
+
+  ;; (setq ivy-extra-directories nil)
+
+  ;; https://github.com/abo-abo/swiper/issues/1068
+  (defun ivy-with-thing-at-point (cmd)
+    (let ((ivy-initial-inputs-alist
+           (list
+            (cons cmd (thing-at-point 'symbol)))))
+      (funcall cmd)))
+
+  (defun ivy-with-thing-at-point-search-symbol (cmd)
+    (let ((ivy-initial-inputs-alist
+           (list
+            (cons cmd (concat "\\_<" (thing-at-point 'symbol) "\\_>")))))
+      (funcall cmd)))
+
+  ;; Swiper multiple cursors
+  (add-to-list 'mc/cmds-to-run-once 'swiper-mc)
+
   ;; Dependencies
   (use-package ivy-hydra))
 
 
-(when (equal larumbe/completion-framework 'ivy)
-  (use-package ivy-youtube
-    :bind (("C-x c y" . ivy-youtube)))
 
+
+
+(when (equal larumbe/completion-framework 'ivy)
   (use-package swiper
     :bind (:map swiper-map
            ("C-r"   . swiper-isearch-C-r)
            ("C-w"   . ivy-yank-word)
            )
     :bind (("C-s"   . swiper)
-           ("M-s ." . swiper-isearch-thing-at-point))
+           ("M-s ." . swiper-symbol-at-point))
     :config
     ;; enable this if you want `swiper' to use it
     (setq search-default-mode #'case-fold-search)
-    )
+
+    (defun swiper-symbol-at-point ()
+      (interactive)
+      (if (thing-at-point 'symbol)
+          (ivy-with-thing-at-point-search-symbol 'swiper)
+        (call-interactively #'swiper))))
 
 
-  ;; INFO: Gave it a try at 10/2021 but it seemed I still was in love with Helm
   (use-package counsel
     :bind (("M-x"     . counsel-M-x)
            ("C-x C-f" . counsel-find-file)
            ("C-x r b" . counsel-bookmark)
-           ("M-g a"   . counsel-ag)
-           ("M-g r"   . counsel-rg)
+           ;; ("M-g a"   . counsel-ag)
+           ("M-g a"   . counsel-ag-thing-at-point)
+           ;; ("M-g r"   . counsel-rg)
+           ("M-g r"   . counsel-rg-thing-at-point)
            ("M-I"     . counsel-imenu)
            ("C-x c /" . counsel-file-jump)
            ("C-x c p" . counsel-list-processes)
            ("C-#"     . counsel-outline)
-           )))
+           ;;  ; Emulate Helm, TODO: Do it at counsel map, not global
+           )
+    :bind (:map counsel-find-file-map
+           ("C-l" . counsel-up-directory))
+    :config
+    (defun counsel-ag-thing-at-point ()
+      (interactive)
+      (ivy-with-thing-at-point 'counsel-ag))
+
+    (defun counsel-rg-thing-at-point ()
+      (interactive)
+      (ivy-with-thing-at-point 'counsel-rg))
+    )
+
+  (use-package ivy-youtube
+    :bind (("C-x c y" . ivy-youtube))))
 
 
 
