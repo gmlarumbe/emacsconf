b368fe84b9520b05dc55fa0a64f6427d8ebc1baf
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2022-04-12 19:10:48 +0200

    Use prog-mode-definitions everywhere to jump to files/URLs

--- lisp-prog/prog/init-prog.el
+++ lisp-prog/prog/init-prog.el
@@ -10,6 +10,8 @@
   ;; INFO: If declaring with :bind, the keybindings will be overriden by major-mode keybindings
   ;;       To override minor-mode keybindings, use :bind*
   ;;       To override major-mode derived keybindings, use prog-mode-hook
+  :bind (("M-." . larumbe/prog-mode-definitions) ; Allow using it in non-prog modes (e.g. open files/URLs with fundamental/conf modes)
+         ("M-?" . larumbe/prog-mode-references)) ; Let them being overriden by major-mode specific keybindings (e.g. `org-open-at-point')
   :hook ((prog-mode . larumbe/prog-mode-hook)
          (prog-mode . larumbe/prog-mode-keys)
          (prog-mode . remove-dos-eol))
