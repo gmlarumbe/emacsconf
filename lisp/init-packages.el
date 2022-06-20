56e998accfb02640fce78b8685fe025390ddbdb5
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2022-06-20 19:21:00 +0200

    Update avy keybindings to more proper ones

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -163,8 +163,8 @@ C-s C-w [C-w] [C-w]... behaviour. "
 
 
 (use-package avy
-  :bind (("C-;" . avy-goto-word-1)
-         ("C-:" . avy-goto-char-2)))
+  :bind (("C-;" . avy-goto-char-2)
+         ("C-:" . avy-goto-word-1)))
 
 
 ;;;; Editing
