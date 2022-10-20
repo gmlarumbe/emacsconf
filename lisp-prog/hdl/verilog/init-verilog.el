852990ead88321cb059d1ea5c38991e5e011d610
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2022-10-21 00:12:23 +0200

    Update verilog init file with external package verilog-ext

--- lisp-prog/hdl/verilog/init-verilog.el
+++ lisp-prog/hdl/verilog/init-verilog.el
@@ -42,12 +42,14 @@
   (setq verilog-date-scientific-format          t)
   (setq verilog-case-fold                     nil) ; Regexps should NOT ignore case
   (setq verilog-align-ifelse                  nil)
+  (setq verilog-indent-ignore-regexp "// \\*") ; Ignore outshine headings
   ;; Verilog AUTO
   (setq verilog-auto-delete-trailing-whitespace t) ; ‘delete-trailing-whitespace’ in ‘verilog-auto’.
   (setq verilog-auto-indent-on-newline          t) ; Self-explaining
   (setq verilog-auto-lineup                   nil) ; other options are 'declarations or 'all
   (setq verilog-auto-newline                  nil)
   (setq verilog-auto-endcomments                t)
+  (setq verilog-auto-wire-comment             nil)
   (setq verilog-minimum-comment-distance        1) ; INFO: (default 10) Only applies to AUTOs, called in `verilog-set-auto-endcomments'
   ;; Alignment
   (setq verilog-align-assign-expr t)
@@ -58,84 +60,35 @@
   (advice-add 'electric-verilog-terminate-line :before-until #'larumbe/newline-advice)) ; Quit *xref* buffer with C-m/RET
 
 
-;; TODO: Checking
 (use-package verilog-ext
-  :straight nil
+  :straight (:host github :repo "gmlarumbe/verilog-ext")
   :after verilog-mode
   :demand
   :bind (:map verilog-mode-map
-         ("M-f"      . verilog-ext-forward-word)
-         ("M-b"      . verilog-ext-backward-word)
-         ;; ("TAB"      . verilog-ext-electric-verilog-tab) ; TODO: Needed?
-
+         ("M-f"     . verilog-ext-forward-word)
+         ("M-b"     . verilog-ext-backward-word)
          ("C-M-d"   . verilog-ext-find-module-instance-fwd)
          ("C-M-u"   . verilog-ext-find-module-instance-bwd)
-         ("M-i"     . verilog-ext-imenu-list)
-
          ("C-M-."   . verilog-ext-jump-to-module-at-point-def)
          ("C-M-/"   . verilog-ext-jump-to-module-at-point-ref)
-
-         ("C-c a"    . verilog-ext-module-at-point-align-ports)
-         ("C-c l"    . verilog-ext-module-at-point-align-params)
-         ("C-c i"    . verilog-ext-module-at-point-indent)
-         ("C-c b"    . verilog-ext-module-at-point-beautify)
-
-         ("C-c C-f"  . verilog-ext-flycheck-mode-toggle)
-
-         ("C-c C-t"  . verilog-ext-hydra/body)
-         )
+         ("M-i"     . verilog-ext-imenu-list)
+         ("C-c a"   . verilog-ext-module-at-point-align-ports)
+         ("C-c l"   . verilog-ext-module-at-point-align-params)
+         ("C-c i"   . verilog-ext-module-at-point-indent)
+         ("C-c b"   . verilog-ext-module-at-point-beautify)
+         ("C-c C-f" . verilog-ext-flycheck-mode-toggle)
+         ("C-c C-t" . verilog-ext-hydra/body)
+         ("<f9>"    . verilog-ext-vhier-current-file))
   :config
   (message "Setting up verilog-ext")
-
   (verilog-ext-which-func-mode)
-
   (setq verilog-ext-flycheck-eldoc-toggle t)
   (setq verilog-ext-flycheck-verible-rules '("-line-length"))
   (verilog-ext-flycheck-setup)
   (verilog-ext-flycheck-set-linter 'verilog-verible)
-
-  (setq verilog-ext-vhier-output-dir (concat (verilog-ext-path-join (projectile-project-root) "vhier/")))
-  )
+  (setq verilog-ext-vhier-output-dir (concat (verilog-ext-path-join (projectile-project-root) "vhier/"))))
 
 
-
-
-;; (use-package verilog-ext
-;;   :straight nil
-;;   :after verilog-mode
-;;   :demand
-;;   :commands (verilog-ext-flycheck-select-linter)
-;;   :hook ((verilog-mode . verilog-ext-hook))
-;;   :bind (:map verilog-mode-map
-;;          ("M-f"      . verilog-ext-forward-word)
-;;          ("M-b"      . verilog-ext-backward-word)
-;;          ("M-i"      . verilog-ext-imenu)
-;;          ("TAB"      . verilog-ext-electric-verilog-tab)
-;;          ("C-M-."    . verilog-ext-find-parent-module)
-;;          ("C-M-n"    . verilog-ext-find-token-fwd)
-;;          ("C-M-p"    . verilog-ext-find-token-bwd)
-;;          ("C-M-u"    . verilog-ext-find-module-instance-bwd)
-;;          ("C-M-d"    . verilog-ext-find-module-instance-fwd)
-;;          ;; ("C-M-h"    . xah-select-current-block) ; TODO: Create an analogous utility, that selects current 'defun'
-;;          ("C-c i"    . verilog-ext-module-at-point-indent)
-;;          ("C-c b"    . verilog-ext-beautify-module-at-point)
-;;          ("C-c B"    . verilog-ext-beautify-module-at-point)
-;;          ("C-c c"    . verilog-ext-toggle-connect-port)
-;;          ("C-c C-c"  . verilog-ext-connect-ports-recursively)
-;;          ("C-c t"    . verilog-ext-time-stamp-work-new-entry)
-;;          ("C-c C-t"  . hydra-verilog/body)
-;;          ("C-c C-p"  . verilog-ext-preprocess)
-;;          ("C-c C-f"  . verilog-ext-flycheck-mode-toggle)
-;;          ("<f9>"     . verilog-ext-vhier-current-file))
-;;   ;; :config
-;;   ;; Dependencies
-;;   ;; (require 'xah-lee-functions)
-;;   ;; (require 'ag)
-;;   ;; Bind chords
-;;   ;; (bind-chord "\\\\" #'verilog-ext-jump-to-module-def-at-point verilog-mode-map)
-;;   ;; (bind-chord "\|\|" #'verilog-ext-find-parent-module verilog-mode-map)
-;;   ;; (key-chord-mode 1)
-;;   )
 
 
 (provide 'init-verilog)
