0c44f7caad82e3d7f0bac4123327419154eb5ce3
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2023-01-20 19:56:52 +0100

    Update emacs script compilation from source for emacs29

--- scripts/configure-emacs-home-libs.sh
+++ scripts/configure-emacs-home-libs.sh
@@ -5,10 +5,10 @@
 # This could be necessary when versions different than the system ones are required
 # or when these are not even installed in a system without root privileges.
 
-PREFIX=$HOME/bin/emacs-28.1
+PREFIX=$HOME/bin/emacs-29
 LIBJANSSON_PATH=${HOME}/bin/jansson-2.14
 LIBGCCJIT_PATH=${HOME}/bin/gcc-12.1.0
-LIBTREESITTER_PATH=${HOME}/bin/tree-sitter
+LIBTREESITTER_PATH=${HOME}/bin/tree-sitter-0.20
 
 # Some basic checks
 [[ -f "./configure" ]] || { echo "Not in proper directory for ./configure"; exit 1; }
@@ -47,6 +47,8 @@ CPPFLAGS+=" -I${LIBGCCJIT_PATH}/include"
 
 
 # Set up libtree-sitter
+TREE_SITTER_CFLAGS="-I${LIBTREESITTER_PATH}/include"
+TREE_SITTER_LIBS="-L${LIBTREESITTER_PATH}/lib"
 LDFLAGS+=" -L${LIBTREESITTER_PATH}/lib"
 CPPFLAGS+=" -I${LIBTREESITTER_PATH}/include"
 LIBS+=" -ltree-sitter"
@@ -58,21 +60,27 @@ echo "JSON_CFLAGS=$JSON_CFLAGS"
 echo "LDFLAGS=$LDFLAGS"
 echo "CPPFLAGS=$CPPFLAGS"
 echo "LIBS=$LIBS"
-export JSON_CFLAGS JSON_LIBS LDFLAGS CPPFLAGS LIBS
+export JSON_CFLAGS JSON_LIBS LDFLAGS CPPFLAGS LIBS TREE_SITTER_CFLAGS TREE_SITTER_LIBS
+
+
+# ./configure will run basic programs to make sure that these libraries are available
+export LD_LIBRARY_PATH=$LIBJANSSON_PATH/lib:$LIBTREESITTER_PATH/lib:$LIBGCCJIT_PATH/lib
 
 
 # Configure command
 set -o pipefail # Save exit code of tee piped command (https://stackoverflow.com/questions/6871859/piping-command-output-to-tee-but-also-save-exit-code-of-command)
 LOG_FILE=configure.log
 echo ""
-./configure --prefix=$PREFIX --with-gif=ifavailable --with-native-compilation --with-json | tee $LOG_FILE
+./configure --prefix=$PREFIX --with-gif=ifavailable --with-native-compilation --with-json --with-tree-sitter | tee $LOG_FILE
 RC=$?
 
+# DANGER: Check visually if these 3 packages are available. Following code somehow hanged the shell...
 # Check if it was successfully setup
-if [ $RC -eq 0 ]; then
-    [[ $(grep "Does Emacs use -ljansson?" $LOG_FILE | awk '{ print $(NF)}') == "yes" ]] || { echo "ERROR with libjansson"; RC=1; }
-    [[ $(grep "Does Emacs have native lisp compiler" $LOG_FILE | awk '{ print $(NF)}') == "yes" ]] || { echo "ERROR with native compiler"; RC=1; }
-    exit $RC
-fi
+# if [ $RC -eq 0 ]; then
+#     [[ $(grep "Does Emacs use -ljansson" $LOG_FILE | awk '{ print $(NF)}') == "yes" ]] || { echo "ERROR with libjansson"; RC=1; }
+#     [[ $(grep "Does Emacs use -ltree-sitter "$LOG_FILE | awk '{ print $(NF)}') == "yes" ]] || { echo "ERROR with tree-sitter"; RC=1; }
+#     [[ $(grep "Does Emacs have native lisp compiler" $LOG_FILE | awk '{ print $(NF)}') == "yes" ]] || { echo "ERROR with native compiler"; RC=1; }
+#     return $RC
+# fi
 
 
