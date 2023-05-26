bb1656582c6d82a34143df30289bb0505ac2c1dd
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2023-05-26 23:15:22 +0200

    Update tcl font-lock

--- lisp-prog/tcl/tcl-utils.el
+++ lisp-prog/tcl/tcl-utils.el
@@ -5,26 +5,42 @@
 
 (require 'tcl)
 
-(defvar larumbe/tcl-braces-face    'larumbe/tcl-braces-face)
-(defvar larumbe/tcl-brackets-face  'larumbe/tcl-brackets-face)
-(defface larumbe/tcl-braces-face   '((t (:foreground "goldenrod")))      "Face for TCL Braces"   :group 'tcl-custom-faces)
-(defface larumbe/tcl-brackets-face '((t (:foreground "dark goldenrod"))) "Face for TCL Brackets" :group 'tcl-custom-faces)
-(defvar larumbe/tcl-font-lock-additional-keywords
-  (list
-   (list "\\(\\[\\|\\]\\)" 0 'larumbe/tcl-braces-face)
-   (list "[()]" 0 'larumbe/tcl-brackets-face)
-   ))
+(defvar larumbe/tcl-parenthesis-face 'larumbe/tcl-parenthesis-face)
+(defface larumbe/tcl-parenthesis-face '((t (:foreground "goldenrod")))
+  "Face for parenthesis ()."
+  :group 'tcl-custom-faces)
 
+(defvar larumbe/tcl-brackets-face 'larumbe/tcl-brackets-face)
+(defface larumbe/tcl-brackets-face '((t (:foreground "dark goldenrod")))
+  "Face for brackets []."
+  :group 'tcl-custom-faces)
+
+(defvar larumbe/tcl-curly-braces-face 'larumbe/tcl-curly-braces-face)
+(defface larumbe/tcl-curly-braces-face
+  '((t (:foreground "DarkGoldenrod2")))
+  "Face for curly braces {}."
+  :group 'tcl-custom-faces)
+
+(defvar larumbe/tcl-punctuation-face 'larumbe/tcl-punctuation-face)
+(defface larumbe/tcl-punctuation-face '((t (:foreground "burlywood")))
+  "Face for punctuation symbols, e.g:
+!,;:?'=<>*"
+  :group 'tcl-custom-faces)
+
+(defconst larumbe/tcl-font-lock-additional-keywords
+  '(("\\(\\[\\|\\]\\)" 0 larumbe/tcl-brackets-face)
+    ("[()]" 0 larumbe/tcl-parenthesis-face)
+    ("[{}]" 0 larumbe/tcl-curly-braces-face)
+    ("[$\\.><=\\*!;]" 0 larumbe/tcl-punctuation-face)
+    ))
 
 (defun larumbe/tcl-font-lock-setup ()
   "Setup font-lock for tcl buffers."
   (font-lock-add-keywords 'tcl-mode larumbe/tcl-font-lock-additional-keywords))
 
-
 (defun larumbe/tcl-mode-hook ()
   "Tcl mode hook."
   (modify-syntax-entry ?$ "."))
-
 
 (defun larumbe/tcl-send-line-or-region-and-step ()
   "Send the current line to the inferior shell and step to the next line.
